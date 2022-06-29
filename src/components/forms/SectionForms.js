69bf2b34fdca49c1206fcd930d91dcf23d95d5b0
SeokHwan <shkim@ddhinc.net>
2022-06-29 16:34:41 +0900

    update

--- src/components/forms/SectionForms.js
+++ src/components/forms/SectionForms.js
@@ -7,36 +7,3 @@ export const SectionWrapper = styled.div`
   align-items: center;
   justify-content: space-around;
 `;
-
-// export const TripleImgWrapper = styled.div`
-//   width: 400px;
-//   height: 338px;
-//   background: url(${(props) => props.url});
-//   background-size: cover;
-//   background-repeat: no-repeat;
-//   padding-top: 280px;
-//   font-size: 15px;
-//   text-align: center;
-//   color: ${palette.gray["0"]};
-// `;
-
-// export const InfoWrapper = styled.div`
-//   width: 417px;
-//   height: 298px;
-//   font-size: 36px;
-// `;
-
-// export const InfoText = styled.div`
-//   margin-bottom: 20px;
-// `;
-
-// export const InfoMedalImg = styled.div`
-//   width: 54px;
-//   height: 54px;
-//   font-size: 14px;
-//   line-height: 22px;
-//   backgroun: url(${(props) => props.url});
-//   background-size: cover;
-//   background-repeat: no-repeat;
-//   color: ${palette.gray["1"]};
-// `;
--- src/containers/Section/SectionContainer.js
+++ src/containers/Section/SectionContainer.js
@@ -11,12 +11,12 @@ const SectionContainer = () => {
     delay: 600,
     delayCnt: 3
   };
-  const [state, setState] = useState(0);
+  const [state, setState] = useState(0); // 애니매이션 순서
   const [init, setInit] = useState(false);
   const [traveler, setTraveler] = useState(700);
   const [review, setReivew] = useState(100);
   const [schedule, setSchedule] = useState(470);
-  const [visible, setVisible] = useState();
+  const [visible, setVisible] = useState(); // state animation setInterval 값
   const info = [
     {
       count: 700,
@@ -42,6 +42,7 @@ const SectionContainer = () => {
           return count;
         }
         callback();
+        // delay부분에 도달하면 count duration 값 변경
         if (delay && count === delay) {
           tick(count + 1, COUNT_TIME.delay / (endpoint - delay)) 
         } else {
@@ -59,14 +60,19 @@ const SectionContainer = () => {
 
   useEffect(() => {
     if (init) {
-      const interval = (COUNT_TIME.default - COUNT_TIME.delay) / (info[0].count - COUNT_TIME.delayCnt);
-      const delay = info[0].count - COUNT_TIME.delayCnt;
+      // 14 ~ 16ms 이하는 브라우져가 인식을 잘 하지 못하기 때문에 끝나는 시점 맞추기 위해 travel 먼저 시작
+      const interval = (COUNT_TIME.default - COUNT_TIME.delay) / (info[0].count - COUNT_TIME.delayCnt); // (기본 시간 - 지연 시간) / (기본 값 - 지연시킬 값)
+      const delay = info[0].count - COUNT_TIME.delayCnt; // 지연될 위치 값
+
       myInterval(() => {
         setTraveler(prev => prev - 1);
       }, interval, delay, info[0].count);
+
+      // interval 800ms(애니매이션 동작시간 700ms + 간격 100ms) 설정
       setVisible(setInterval(() => {
         setState(prev => (prev + 1));
       }, 800));
+
       return () => clearInterval(visible);
     }
   }, [init]);
